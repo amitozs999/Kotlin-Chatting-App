af981b0b1ed092696e6d4e7b9e92e1a0843a3425
Amitoz singh <52659877+amitozs999@users.noreply.github.com>
2019-11-24 01:34:25 +0530

    sending data from server to firebase

--- myserver/firebase/userserver.js
+++ myserver/firebase/userserver.js
@@ -29,11 +29,55 @@ var userAccountrequests=(io)=>{
 };
 function registerUser(socket,io){
     socket.on('userData',(data)=>{
-        console.log(data.email);
-        console.log(data.userName);
-        console.log(data.password);
-    })
-}
+      admin.auth().createUser({
+        email:data.email,
+        displayName:data.userName,
+        password:data.password
+      })
+      .then((userRecord)=>{
+        console.log('User was registered successfully');
+        var db = admin.database();
+        var ref = db.ref('users');
+        var userRef = ref.child(encodeEmail(data.email));
+        var date = {
+          data:admin.database.ServerValue.TIMESTAMP
+        };
+  
+        userRef.set({
+          email:data.email,
+          userName:data.userName,
+          userPicture:'',
+          dateJoined:date,
+          hasLoggedIn:false
+        });
+  
+        Object.keys(io.sockets.sockets).forEach((id)=>{
+          if (id == socket.id) {
+            var message = {
+              text:'Success'
+            }
+            io.to(id).emit('message',{message});
+          }
+        });
+  
+  
+      }).catch((error)=>{
+        Object.keys(io.sockets.sockets).forEach((id)=>{
+          console.log(error.message);
+          if (id == socket.id) {
+            var message = {
+              text:error.message
+            }
+            io.to(id).emit('message',{message});
+          }
+        });
+      });
+    });
+  }
+  function encodeEmail(email){
+    return email.replace('.',',');
+  }
+  
 module.exports={
     userAccountrequests
 };
